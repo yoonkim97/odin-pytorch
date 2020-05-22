19992a074a1f0cd494927d8a9217e96c49896706
Yoon Kim <yoonkim752@gmail.com>
2020-05-22 05:08:41 +0100

    Fixed model loading

--- code/cal.py
+++ code/cal.py
@@ -57,8 +57,7 @@ def recursion_change_bn(module):
     return module
 
 def test(nnName, dataName, CUDA_DEVICE, epsilon, temperature):
-    model = DenseNetBC_50_12()
-    model.load_state_dict(torch.load("../models/{}.pth".format(nnName)))
+    model = model.load_state_dict(torch.load("../models/{}.pth".format(nnName)))
     optimizer1 = optim.SGD(model.parameters(), lr=0, momentum=0)
     for i, (name, module) in enumerate(model._modules.items()):
         module = recursion_change_bn(model)
