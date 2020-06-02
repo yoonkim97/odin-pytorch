1ef70c8a59ee5f648074da5c9dc4f9bb70618e9c
Yoon Kim <jyk416@ic.ac.uk>
2020-06-03 00:19:49 +0900

    Back to both in and out

--- code/calData.py
+++ code/calData.py
@@ -24,59 +24,59 @@ def testData(net1, criterion, CUDA_DEVICE, testloader10, testloader, nnName, noi
     f2 = open("./softmax_scores/confidence_Base_Out.txt", 'w')
     g1 = open("./softmax_scores/confidence_Our_In.txt", 'w')
     g2 = open("./softmax_scores/confidence_Our_Out.txt", 'w')
-    N = 400
+    # N = 400
     # if dataName == "iSUN": N = 8925
     print("Processing in-distribution images")
 ########################################In-distribution###########################################
-    # for j, data in enumerate(testloader10):
-    #     # if j<1000: continue
-    #     images, _ = data
-    #
-    #     inputs = Variable(images.cuda(CUDA_DEVICE), requires_grad = True)
-    #     outputs = net1(inputs)
-    #
-    #
-    #     # Calculating the confidence of the output, no perturbation added here, no temperature scaling used
-    #     nnOutputs = outputs.data.cpu()
-    #     nnOutputs = nnOutputs.numpy()
-    #     nnOutputs = nnOutputs[0]
-    #     nnOutputs = nnOutputs - np.max(nnOutputs)
-    #     nnOutputs = np.exp(nnOutputs)/np.sum(np.exp(nnOutputs))
-    #     f1.write("{}, {}, {}\n".format(temper, noiseMagnitude1, np.max(nnOutputs)))
-    #
-    #     # Using temperature scaling
-    #     outputs = outputs / temper
-	#
-    #     # Calculating the perturbation we need to add, that is,
-    #     # the sign of gradient of cross entropy loss w.r.t. input
-    #     maxIndexTemp = np.argmax(nnOutputs)
-    #     labels = Variable(torch.LongTensor([maxIndexTemp]).cuda(CUDA_DEVICE))
-    #     loss = criterion(outputs, labels)
-    #     loss.backward()
-    #
-    #     # Normalizing the gradient to binary in {0, 1}
-    #     gradient =  torch.ge(inputs.grad.data, 0)
-    #     gradient = (gradient.float() - 0.5) * 2
-    #     # Normalizing the gradient to the same space of image
-    #     gradient[0][0] = (gradient[0][0] )/(63.0/255.0)
-    #     gradient[0][1] = (gradient[0][1] )/(62.1/255.0)
-    #     gradient[0][2] = (gradient[0][2])/(66.7/255.0)
-    #     # Adding small perturbations to images
-    #     tempInputs = torch.add(inputs.data,  -noiseMagnitude1, gradient)
-    #     outputs = net1(Variable(tempInputs))
-    #     outputs = outputs / temper
-    #     # Calculating the confidence after adding perturbations
-    #     nnOutputs = outputs.data.cpu()
-    #     nnOutputs = nnOutputs.numpy()
-    #     nnOutputs = nnOutputs[0]
-    #     nnOutputs = nnOutputs - np.max(nnOutputs)
-    #     nnOutputs = np.exp(nnOutputs)/np.sum(np.exp(nnOutputs))
-    #     g1.write("{}, {}, {}\n".format(temper, noiseMagnitude1, np.max(nnOutputs)))
-    #     # if j % 100 == 99:
-    #     print("{:4}/{:4} images processed, {:.1f} seconds used.".format(j+1, N, time.time()-t0))
-    #     t0 = time.time()
-    #
-    #     # if j == N - 1: break
+    for j, data in enumerate(testloader10):
+        # if j<1000: continue
+        images, _ = data
+
+        inputs = Variable(images.cuda(CUDA_DEVICE), requires_grad = True)
+        outputs = net1(inputs)
+
+
+        # Calculating the confidence of the output, no perturbation added here, no temperature scaling used
+        nnOutputs = outputs.data.cpu()
+        nnOutputs = nnOutputs.numpy()
+        nnOutputs = nnOutputs[0]
+        nnOutputs = nnOutputs - np.max(nnOutputs)
+        nnOutputs = np.exp(nnOutputs)/np.sum(np.exp(nnOutputs))
+        f1.write("{}, {}, {}\n".format(temper, noiseMagnitude1, np.max(nnOutputs)))
+
+        # Using temperature scaling
+        outputs = outputs / temper
+
+        # Calculating the perturbation we need to add, that is,
+        # the sign of gradient of cross entropy loss w.r.t. input
+        maxIndexTemp = np.argmax(nnOutputs)
+        labels = Variable(torch.LongTensor([maxIndexTemp]).cuda(CUDA_DEVICE))
+        loss = criterion(outputs, labels)
+        loss.backward()
+
+        # Normalizing the gradient to binary in {0, 1}
+        gradient =  torch.ge(inputs.grad.data, 0)
+        gradient = (gradient.float() - 0.5) * 2
+        # Normalizing the gradient to the same space of image
+        gradient[0][0] = (gradient[0][0] )/(63.0/255.0)
+        gradient[0][1] = (gradient[0][1] )/(62.1/255.0)
+        gradient[0][2] = (gradient[0][2])/(66.7/255.0)
+        # Adding small perturbations to images
+        tempInputs = torch.add(inputs.data,  -noiseMagnitude1, gradient)
+        outputs = net1(Variable(tempInputs))
+        outputs = outputs / temper
+        # Calculating the confidence after adding perturbations
+        nnOutputs = outputs.data.cpu()
+        nnOutputs = nnOutputs.numpy()
+        nnOutputs = nnOutputs[0]
+        nnOutputs = nnOutputs - np.max(nnOutputs)
+        nnOutputs = np.exp(nnOutputs)/np.sum(np.exp(nnOutputs))
+        g1.write("{}, {}, {}\n".format(temper, noiseMagnitude1, np.max(nnOutputs)))
+        # if j % 100 == 99:
+        print("{:4}/{:4} images processed, {:.1f} seconds used.".format(j+1, 84817, time.time()-t0))
+        t0 = time.time()
+
+        # if j == N - 1: break
 
 
     t0 = time.time()
