d00cabd8872b2fc2e0f5e666435784ef98009e04
Yoon Kim <jyk416@ic.ac.uk>
2020-06-03 09:35:01 +0900

    Chage N

--- code/calData.py
+++ code/calData.py
@@ -24,7 +24,7 @@ def testData(net1, criterion, CUDA_DEVICE, testloader10, testloader, nnName, noi
     f2 = open("./softmax_scores/confidence_Base_Out.txt", 'w')
     g1 = open("./softmax_scores/confidence_Our_In.txt", 'w')
     g2 = open("./softmax_scores/confidence_Our_Out.txt", 'w')
-    # N = 400
+    N = 300
     # if dataName == "iSUN": N = 8925
     print("Processing in-distribution images")
 ########################################In-distribution###########################################
@@ -73,10 +73,10 @@ def testData(net1, criterion, CUDA_DEVICE, testloader10, testloader, nnName, noi
         nnOutputs = np.exp(nnOutputs)/np.sum(np.exp(nnOutputs))
         g1.write("{}, {}, {}\n".format(temper, noiseMagnitude1, np.max(nnOutputs)))
         # if j % 100 == 99:
-        print("{:4}/{:4} images processed, {:.1f} seconds used.".format(j+1, 84817, time.time()-t0))
+        print("{:4}/{:4} images processed, {:.1f} seconds used.".format(j+1, N, time.time()-t0))
         t0 = time.time()
 
-        # if j == N - 1: break
+        if j == N - 1: break
 
 
     t0 = time.time()
